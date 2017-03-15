e9ca2fd1bf497b53862e770e21aef74853ce6c03
crackdasmina <daniel.silva.prg@gmail.com>
2017-03-15 12:33:52 +0000

    Updated 2017-03-15/12:33

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 /*
   gulpfile.js
 */
-
+'use strict';
 var gulp=require('gulp');
 var sass=require('gulp-sass');
+var sourcemaps=require('gulp-sourcemaps');
 
 gulp.task('sass',function(){
-  return gulp.src('app/sass/*.scss')
-    .pipe(sass())
+  return gulp.src('app/sass/style.scss')
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('app/css'));
 });
